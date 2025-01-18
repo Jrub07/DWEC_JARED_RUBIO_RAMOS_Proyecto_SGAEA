9267bcfb138979ec8dbb7069db6fd4dad830f2c3
jrub07 <jrubram179@g.educaand.es>
2025-01-18 18:34:44 +0100

    babel y polyfill hechos

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports={
+entry: './main.js',
+output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename:"[name].[contenthash].js",
+    publicPath: ""
+},
+
+mode: "production",
+module: {
+    rules:[
+        {
+            use: 'babel-loader',
+            test:/.(js|jsx)$/,
+            exclude: /node_modules/
+        }
+    ]
+},
+
+resolve:{
+    extensions:[".js", ".jsx", ".json"],
+
+}
+}
