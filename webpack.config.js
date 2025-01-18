cde12e95614f912bb20e9b6c87c67bd771a80a14
jrub07 <jrubram179@g.educaand.es>
2025-01-18 18:45:27 +0100

    ahora crea el index con el bundle creado

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,30 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports={
-entry: './main.js',
-output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename:"[name].[contenthash].js",
-    publicPath: ""
-},
-
-mode: "production",
-module: {
-    rules:[
-        {
-            use: 'babel-loader',
-            test:/.(js|jsx)$/,
-            exclude: /node_modules/
-        }
+module.exports = {
+    mode: "production",
+    entry: './main.js', // Ajusta la entrada al archivo principal
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                use: 'babel-loader',
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".js", ".jsx", ".json"]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: "./index.html",
+        })
     ]
-},
-
-resolve:{
-    extensions:[".js", ".jsx", ".json"],
-
-}
-}
+};
