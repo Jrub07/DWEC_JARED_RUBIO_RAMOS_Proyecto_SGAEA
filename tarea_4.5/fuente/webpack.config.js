6d06c2b93f994a421c5588652c999d45c01f6cfb
jrub07 <jrubram179@g.educaand.es>
2025-02-10 18:18:44 +0100

    codigo reoganizado a espera de readme

---
+++ tarea_4.5/fuente/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'production', // Modo de producción
+  entry: path.resolve(__dirname, 'js/main.js'), 
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'html/index.html'), 
+    }),
+  ],
+};
